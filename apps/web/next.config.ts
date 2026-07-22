49001d9c0c4651750525f11f68e6968d1019d22a
Paulo Vinícius Toledo <pvvtoledo@gmail.com>
2026-07-21 21:13:40 -0300

    fix: github actions

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    transpilePackages: ["@duely/shared"]
+  transpilePackages: ["@duely/shared"],
 };
 
 export default nextConfig;
